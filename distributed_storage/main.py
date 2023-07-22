2184ea7e53fe51ef30d1cecec184c87eaa99f044
Yaroslav <y.kivaev@tinkoff.ru>
2023-07-22 21:43:54 +0300

    Redis storage were introduced

--- distributed_storage/main.py
+++ distributed_storage/main.py
@@ -1,17 +1,24 @@
-from argparse import ArgumentParser
-
-import os
-
 from distributed_storage.environment_variables.ExposedGrpcPort import ExposedGrpcPort
 from distributed_storage.environment_variables.InnerXmlRpcPort import InnerXmlRpcPort
 from distributed_storage.environment_variables.OtherNodesPorts import OtherNodesPorts
+from distributed_storage.environment_variables.redis_nodes_suffix import RedisNodesSuffix
+from distributed_storage.storage.redis_storage import RedisStorage
 from distributed_storage.storage_servers.outside_storage_server import OutsideStorageServer
-from storage.dictionary_storage import DictionaryStorage
 from storage_api import StorageAPI
 from storage_servers.interconnect_storage_server import InterConnectStorageServer
+from redis.cluster import ClusterNode
 
 if __name__ == '__main__':
-    storage = DictionaryStorage()
+    redis_nodes_suffix = str(RedisNodesSuffix())
+    print(redis_nodes_suffix)
+    storage = RedisStorage(
+        startup_nodes=[ClusterNode(host="redis-node-1"+redis_nodes_suffix, port=6379),
+                       ClusterNode(host="redis-node-2"+redis_nodes_suffix, port=6379),
+                       ClusterNode(host="redis-node-3"+redis_nodes_suffix, port=6379),
+                       ClusterNode(host="redis-node-4"+redis_nodes_suffix, port=6379),
+                       ClusterNode(host="redis-node-5"+redis_nodes_suffix, port=6379),
+                       ClusterNode(host="redis-node-6"+redis_nodes_suffix, port=6379),
+                       ])
     InterConnectStorageServer(InnerXmlRpcPort(), storage).run()
     StorageAPI(
         f'0.0.0.0:{ExposedGrpcPort().value()}',
